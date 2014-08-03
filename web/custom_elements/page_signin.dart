bc0f879fb2a29738a37c81dba40bdef331d9b124
olituks <olituks@gmail.com>
2014-08-03 16:10:45 +0200

    add HttpRequest POST method

--- web/custom_elements/page_signin.dart
+++ web/custom_elements/page_signin.dart
@@ -3,6 +3,8 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:polymer/polymer.dart';
+import 'dart:html';
+import 'dart:convert';
 
 @CustomTag('signin-element')
 class Signin extends PolymerElement {
@@ -14,26 +16,41 @@ class Signin extends PolymerElement {
   
   ready() {
     super.ready();
-    _saveData();
+    _saveData2();
   }
   
   void _saveData() {
     HttpRequest request = new HttpRequest(); // create a new XHR
-    
+  
     // add an event handler that is called when the request finishes
     request.onReadyStateChange.listen((_) {
       if (request.readyState == HttpRequest.DONE &&
           (request.status == 200 || request.status == 0)) {
         // data saved OK.
-        print(request.responseText); // output the response from the server
+        print(request.responseText);
       }
     });
   
     // POST the data to the server
-    var url = "http://127.0.0.1:8080/programming-languages";
+    var url = "http://127.0.0.1:8080/savedata";
     request.open("POST", url, async: false);
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   
-    String jsonData = '{"language":"dart"}'; // etc...
-    request.send(jsonData); // perform the async POST
+    /*String jsonData = JSON.encode({"language":"dart2"});*/
+    String jsonData = "language=dart2";
+    request.send(jsonData);
+  }
+  
+  void _saveData2() {
+    String url = "http://127.0.0.1:8080/savedata";
+    HttpRequest request = new HttpRequest() 
+      ..open("POST", url, async: true)
+      ..setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+      ..responseType = "arraybuffer";
+
+    
+    String jsonData = JSON.encode({"test":"valuetest1"});
+    String datas = "datas=$jsonData";
+    request.send(datas);
   }
 }
